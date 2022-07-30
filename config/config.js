ded22c7fd641fbf816562aee4593e28aeab9f540
Erwin <ibarra.erwin09@gmail.com>
2022-07-30 18:00:49 +0800

    ready for deployment

--- config/config.js
+++ config/config.js
@@ -15,18 +15,11 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
+  username: process.env.CLEAR_DB_USERNAME,
+  password: process.env.CLEAR_DB_PASSWORD,
+  database: process.env.CLEAR_DB_DATABASE,
+  host: process.env.CLEAR_DB_HOST,
+  dialect: "mysql",
+}
 };
 
-// production: {
-//   username: process.env.CLEAR_DB_USERNAME,
-//   password: process.env.CLEAR_DB_PASSWORD,
-//   database: process.env.CLEAR_DB_DATABASE,
-//   host: process.env.CLEAR_DB_HOST,
-//   dialect: "mysql",
-// },
